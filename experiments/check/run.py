7f9ecfca934f952ad055cd4dc47a2539c2ff258c
unonao <naokiuno634@gmail.com>
2023-12-10 02:20:36 +0000

    update

--- experiments/check/run.py
+++ experiments/check/run.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import time
 from pathlib import Path
@@ -10,14 +11,14 @@ import utils
 
 
 @hydra.main(version_base=None, config_path=".", config_name="config")
-def my_app(cfg: DictConfig) -> None:
+def main(cfg: DictConfig) -> None:
     runtime_choices = HydraConfig.get().runtime.choices
     exp_name = f"{Path(sys.argv[0]).parent.name}/{runtime_choices.exp}"
 
     print(f"exp_name: {exp_name}")
-    ouput_path = Path(cfg.dir.output_dir) / exp_name
-    print(f"ouput_path: {ouput_path}")
-    # os.makedirs(output_path, exist_ok=True)
+    output_path = Path(cfg.dir.exp_dir) / exp_name
+    print(f"ouput_path: {output_path}")
+    os.makedirs(output_path, exist_ok=True)
 
     with utils.timer("print"):
         time.sleep(1.1)
@@ -25,4 +26,5 @@ def my_app(cfg: DictConfig) -> None:
 
 
 if __name__ == "__main__":
-    my_app()
+    main()
+
